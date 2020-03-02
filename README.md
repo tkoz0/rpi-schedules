869a7eb427dd384fb5427c18c23a008a9478a3a9
TKoz Zero <tkozlowski1999@gmail.com>
2020-03-02 00:34:23 -0500

    wrote page scrubber to extract data table

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rpi-schedules
 class schedules scraped from sis.rpi.edu
+
+saving pages to use
+1. log into sis -> student menu -> class search
+2. pick a semester, select all subjects, and search for sections
+3. save the page (html only)
+
+processing pages
+1. place raw pages in a directory and make a new directory for scrubbed output
+2. run ``python3 page_scrubber.py <input dir> <output dir>``
+
---
+++ page_scrubber.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import sys
+import os
+
+# usage: page_scrubber.py <input dir> <output dir>
+inDir = sys.argv[1]
+outDir = sys.argv[2]
+if inDir[-1] != '/': inDir += '/'
+if outDir[-1] != '/': outDir += '/'
+
+for file in os.listdir(inDir):
+    
+    # reduce page to just the data table
+    bs4page = BeautifulSoup(open(inDir+file).read(),'html.parser')
+    dataTable = bs4page.find_all('table',class_='datadisplaytable')[0]
+    html = bs4page.find_all('html')[0]
+    html.contents = ['\n',dataTable,'\n']
+    table = bs4page.find_all('table',class_='datadisplaytable')[0]
+    table.contents = table.contents[2:] # remove "sections found" text
+    
+    # write scrubbed file
+    outFile = open(outDir+file,'w')
+    outFile.write(str(bs4page))
+    outFile.close()
